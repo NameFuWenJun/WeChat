058b92c8dcb8b96faf052eac73296b1970a92b33
Wenjun Fu (符文俊)-云服务集团 <fuwenjun01@inspur.com>
2018-09-02 11:30:43 +0800

    微信小程序后端以及前台demo

---
+++ fisherBackground/src/main/java/com/fisher/FisherBackgroundApplication.java
@@ -0,0 +1,12 @@
+package com.fisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisherBackgroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisherBackgroundApplication.class, args);
+	}
+}
